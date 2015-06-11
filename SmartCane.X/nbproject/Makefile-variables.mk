57ad3bdf4a952764c7685f8e2610231dad1256a5
Sceap <jeremie.vandersande@gmail.com>
2015-06-11 17:05:45 -0300

    MPLABX Project
    Create an Empty MPLABX Project to be populated with the final code

---
+++ SmartCane.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=SmartCane.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/SmartCane.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=smartcane.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/smartcane.x.tar
